0dbe02903687693a4fc31d8c82cec955f92e1ebf
Arooj Fatima <arooj.fatima@brainxtech.com>
2024-12-11 14:51:04 +0500

    Turbo Streams and security

--- Gemfile
+++ Gemfile
@@ -65,4 +65,6 @@ group :test do
 end
 
 gem "simple_form", "~> 5.1.0"
+gem "devise", "~> 4.9b"
+
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :quotes
 
@@ -11,6 +12,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "quotes#index"
+  root "pages#home"
 
 end
